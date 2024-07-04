9b0c486d8850c561977daf01d95d352166dd90c3
Tithira Mojitha Ranasinghe <127576267+MojithaR@users.noreply.github.com>
2024-07-04 14:17:15 +0530

    Add files via upload

---
+++ 100WPF_FirstProject/01WPF_FirstProject/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace _01WPF_FirstProject
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
