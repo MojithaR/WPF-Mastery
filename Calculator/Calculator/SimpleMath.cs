e658aacbb54f40ced869f35df22e558a6fa1c697
Tithira Mojitha Ranasinghe <127576267+MojithaR@users.noreply.github.com>
2024-07-05 12:43:55 +0530

    Add files via upload

---
+++ Calculator/Calculator/SimpleMath.cs
@@ -0,0 +1,23 @@
+﻿namespace Calculator
+{
+    public class SimpleMath
+    {
+        public static double Add(double numberOne, double numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        public static double Minus(double numberOne, double numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+        public static double Multiple(double numberOne, double numberTwo)
+        {
+            return numberOne * numberTwo;
+        }
+        public static double Divide(double numberOne, double numberTwo)
+        {
+            return numberOne / numberTwo;
+        }
+    }
+}
